af1a35ba45a384bdc4d454e7e9dd0904717db2af
Valeria <vozzheva@mail.ru>
2020-07-29 10:08:46 +0300

    Добавляет фильтры

--- src/main.js
+++ src/main.js
@@ -9,6 +9,29 @@ const createSiteMenuTemplate = () => {
     );
   };
 
+  const createSiteFiltersTemplate = () => {
+    return (
+      `<form class="trip-filters" action="#" method="get">
+      <div class="trip-filters__filter">
+        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+        <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      </div>
+
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`
+      );
+    };
+
 const render = (container, template, place) => {
     container.insertAdjacentHTML(place, template);
   };
@@ -16,5 +39,7 @@ const render = (container, template, place) => {
 const siteHeaderElement = document.querySelector(`.page-header`);
 const siteControlsElement = siteHeaderElement.querySelector(`.trip-controls`);
 const menuHeadingElement = siteControlsElement.querySelector(`h2:first-child`);
+const filtersHeadingElement = siteControlsElement.querySelector(`h2:nth-child(2)`);
 
 render(menuHeadingElement, createSiteMenuTemplate(), `afterend`);
+render(filtersHeadingElement, createSiteFiltersTemplate(), `afterend`);
