93409a55c58d87ba365dfe005b97c7e81c94281c
Valeria <vozzheva@mail.ru>
2020-08-26 14:47:26 +0300

    Bug with menu rendering

--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -2,8 +2,7 @@ import {createElement} from "../util.js";
 
 const createSiteMenuTemplate = () => {
   return (
-    `<h2 class="visually-hidden">Switch trip view</h2>
-    <nav class="trip-controls__trip-tabs  trip-tabs">
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
       <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
       <a class="trip-tabs__btn" href="#">Stats</a>
     </nav>`
