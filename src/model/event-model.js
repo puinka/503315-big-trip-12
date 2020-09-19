56d37cd0fc70399cfe3449f7cefde27a20d3325a
Valeria <vozzheva@mail.ru>
2020-09-19 10:08:21 +0100

    Observer

---
+++ src/model/event-model.js
@@ -0,0 +1,16 @@
+import Observer from "../utils/observer.js";
+
+export default class Event extends Observer {
+  constructor() {
+    super();
+    this._events = [];
+  }
+
+  setEvents(events) {
+    this._events = events.slice();
+  }
+
+  getEvents() {
+    return this._events;
+  }
+}
